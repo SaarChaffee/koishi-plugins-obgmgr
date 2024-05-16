668a3993c4bb3739569efffd3da1675f42639aeb
霞飛 <saarchaffee@qq.com>
2024-05-16 15:28:06 +0800

    feat: group request limit

---
+++ packages/group-request-limit/readme.md
@@ -0,0 +1,5 @@
+# group-request-limit
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-group-request-limit?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-group-request-limit)
+
+用于 obgmgr 的加群限制
---
+++ packages/group-request-limit/src/index.ts
@@ -0,0 +1,26 @@
+import { Context, Schema } from 'koishi'
+
+export const name = 'group-request-limit'
+
+export interface Config {
+  list: string[]
+  groups: string[]
+}
+
+export const Config: Schema<Config> = Schema.object({
+  groups: Schema.array(Schema.string()).description('群组生效白名单'),
+  list: Schema.array(Schema.string()).description('黑名单列表'),
+})
+
+export function apply(ctx: Context, config: Config) {
+  if (config.list.length > 0) {
+    ctx.guild(...config.groups).on('guild-member-request', async (meta) => {
+      if (process.env.NODE_ENV === 'development') {
+        ctx.logger.info(meta)
+      }
+      if (config.list.includes(meta.userId)) {
+        await meta.bot.handleGuildMemberRequest(meta.messageId, false, '黑名单自动拒绝。')
+      }
+    })
+  }
+}
